01ea5c815bb7366e0388d30f2170320616389957
jvillegasl <jvillegasl@uni.pe>
2023-07-24 00:05:26 -0500

    keypoints loss fn implemented

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,18 @@
+import torch
 import torch.nn.functional as F
 
 
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+def kpts_loss(output, target):
+    pred_kpts = output['pred_kpts']
+    pred_visibility = output['pred_visibility']
+
+    tgt_raw = torch.cat([v['keypoints'] for v in target])
+    tgt_kpts = tgt_raw[:, :, :2]
+    tgt_visibility = tgt_raw[:, :, 2:].flatten(1)
+
+    loss_kpts = F.l1_loss(pred_kpts, tgt_kpts, reduction='mean')
+    loss_visibility = F.binary_cross_entropy(pred_visibility, tgt_visibility)
+
+    loss = loss_kpts + loss_visibility
+
+    return loss
