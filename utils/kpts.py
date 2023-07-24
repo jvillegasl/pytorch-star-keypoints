e8aee4d9bd49216380a0067a0dbb38234fdf0461
jvillegasl <jvillegasl@uni.pe>
2023-07-23 23:06:29 -0500

    keypoints util fns

---
+++ utils/kpts.py
@@ -0,0 +1,30 @@
+import torch
+from torch import Tensor
+import matplotlib.pyplot as plt
+
+
+def rescale_kpt(kpt: Tensor, size):
+    img_w, img_h = size
+
+    rescaled_kpt = kpt * torch.tensor([img_w, img_h, 1], dtype=torch.float32)
+
+    return rescaled_kpt
+
+
+def get_kpt_mark(rescaled_kpt: Tensor, color):
+    x, y, _ = rescaled_kpt
+
+    circle = plt.Circle((x, y), 3, color=color)
+
+    return circle
+
+
+def get_kpt_text(rescaled_kpt: Tensor, kpt_class: str):
+    x, y, _ = rescaled_kpt
+
+    txt = kpt_class
+
+    text = plt.text(x, y, txt, fontsize=7, bbox=dict(
+        facecolor='yellow', alpha=0.5))
+
+    return text
